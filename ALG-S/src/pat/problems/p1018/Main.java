e56a949286ead294690aedec8655749d43820779
wagsenyuan <wang.senyuan@gmail.com>
2013-10-19 12:10:48 +0800

    PAT 1018.... I thought it should take max back, but actually, it requires to take min back...how did this happen?

--- ALG-S/src/pat/problems/p1018/Main.java
+++ ALG-S/src/pat/problems/p1018/Main.java
@@ -3,7 +3,6 @@ package pat.problems.p1018;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.PriorityQueue;
 
 public class Main {
 
@@ -37,15 +36,14 @@ public class Main {
 				matrix[w][v] = l;
 			}
 			
-			G g = new G(matrix, n + 1);
-			BFS bfs = new BFS(g, 0, t, capacity, full);
+//			G g = new G(matrix, n + 1);
+			BFS bfs = new BFS(matrix, n + 1, 0, t, capacity, full);
 			int sent = bfs.sent();
 			int back = bfs.back();
 			int[] path = bfs.path();
 			System.out.print(sent + " ");
-			int i = path.length - 1;
-			System.out.print(path[i]);
-			for(i = i - 1; i >= 0; i--) {
+			System.out.print(path[0]);
+			for(int i = 1; i < path.length; i++) {
 				System.out.print("->" + path[i]);
 			}
 			System.out.println(" " + back);
@@ -57,121 +55,128 @@ public class Main {
 
 }
 
-
-class G {
-	private int[][] matrix;
-	final int n;
-	public G(int[][] matrix, int n) {
-		this.matrix = matrix;
-		this.n = n;
-	}
-	
-	public int[] adj(int v) {
-		return matrix[v];
-	}
-}
-
 class BFS {
-	private G g;
+	private int[][] g;
+	private int n;
 	private int s, t;
-	private int[] edgeTo;
+	private int[][] pc; //parent-child relationship if pc[i][j], 1 for j is i's child, 0 for no relation, -1 for i is j's parent 
 	private Node[] nodes;
+	private int[] path;
 	int perfect;
-	public BFS(G g, int s, int t, int[] capacity, int full) {
+	int sent = Integer.MAX_VALUE, back;
+	public BFS(int[][] g, int n, int s, int t, int[] capacity, int full) {
 		this.g = g;
+		this.n = n;
 		this.s = s;
 		this.t = t;
-		this.edgeTo = new int[g.n];
-		this.edgeTo[s] = -1;
-		nodes = new Node[g.n];
-		for(int i = 0; i < g.n; i++) {
+		nodes = new Node[n];
+		for(int i = 0; i < n; i++) {
 			nodes[i] = new Node(i, Integer.MAX_VALUE, capacity[i]);
 		}
-		nodes[s].distance = 0;
+		nodes[this.s].distance = 0;
 		this.perfect = full >> 1;
-		
+		nodes[this.s].capacity = perfect;
+		this.pc = new int[n][n];
+		this.path = new int[n];
 		bfs();
+		dfs(s, 0, 0, new int[n], 0);
 	}
 	
 	
 	private void bfs() {
-		PriorityQueue<Node> pq = new PriorityQueue<Node>();
-		pq.offer(nodes[s]);
-		while(!pq.isEmpty()) {
-			Node v = pq.poll();
-			if(v.marked) {
-				continue;
+		while(true) {
+			//find the nearest node
+			Node v = null;
+			int minDistance = Integer.MAX_VALUE;
+			for(int i = 0; i < n; i++) {
+				Node node = nodes[i];
+				if(!node.marked && node.distance < minDistance) {
+					minDistance = node.distance;
+					v = node;
+				}
 			}
 			
-			v.marked = true;
-			if(v.index == t) {
+			if(v == null) {
 				break;
 			}
 			
-			int[] adj = g.adj(v.index);
+			if(v.index == t) {
+				break;
+			}
+			v.marked = true;
+			int[] adj = g[v.index];
 			for(int i = 0; i < adj.length; i++) {
-				Node w = nodes[i];
-				if(adj[i] == 0 || w.marked) {
+				if(nodes[i].marked || g[v.index][i] == 0) {
 					continue;
 				}
-				if(v.distance + adj[i] <= w.distance) {
-					//a new shortest path found;
-					int send = 0, back = 0;
-					if(w.capacity > perfect) {
-						back = v.back + (w.capacity -perfect);
-						send = v.send;
-					} else if(w.capacity < perfect) {
-						if(v.back < (perfect - w.capacity)) {
-							back = 0;
-							send = (perfect - w.capacity) - v.back + v.send;
-						} else {
-							back = v.back - (perfect - w.capacity);
-							send = v.send;
-						}
-					} else {
-						send = v.send;
-						back = v.back;
-					}
-					
-					if(v.distance + adj[i] < w.distance || (send < w.send || (send == w.send && back > w.back))) {
-						//update the new path;
-						edgeTo[i] = v.index;
-						w.send = send;
-						w.back = back;
-						w.distance = v.distance + adj[i];
-					} 
-				} 
 				
-				pq.offer(w);
+				Node w = nodes[i];
+				if(v.distance + adj[i] <= w.distance) {
+					if(v.distance + adj[i] < w.distance) {
+						int[] ps = pc[i];
+						for(int k = 0; k < ps.length; k++) {
+							if(ps[k] < 0) {
+								//i is k's child
+								pc[i][k] = 0;
+								pc[k][i] = 0;
+								//then no relation
+							}
+						}
+					}
+					pc[v.index][i] = 1;
+					pc[i][v.index] = -1;
+					w.distance = v.distance + adj[i];
+					//update the relation
+				} 
+			}
+		}
+	}
+	
+	private void dfs(final int x, final int sent, final int back, final int[] path, final int pos) {
+		path[pos] = x;
+		Node v = nodes[x];
+		int sentToNext = 0;
+		int backToNext = 0;
+		if(back + v.capacity < perfect) {
+			backToNext = 0;
+			sentToNext = sent + (perfect - (back + v.capacity));
+		} else if(back + v.capacity >= perfect) {
+			backToNext = (back + v.capacity - perfect);
+			sentToNext = sent;
+		}
+		
+		if(x == t) {
+			if(sentToNext < this.sent || (sentToNext == this.sent && backToNext < this.back)) {
+				this.sent = sentToNext;
+				this.back = backToNext;
+				this.path = Arrays.copyOf(path, pos + 1);
+			}
+		}
+		
+		
+		int[] children = pc[x];
+		for(int i = 0; i < children.length; i++) {
+			if(children[i] > 0) {
+				dfs(i, sentToNext, backToNext, path, pos + 1);
 			}
 		}
 	}
 	
 	public int sent() {
-		Node e = nodes[t];
-		return e.send;
+		return sent;
 	}
 	
 	public int back() {
-		Node e = nodes[t];
-		return e.back;
+		return back;
 	}
 	
 	public int[] path() {
-		int[] path = new int[edgeTo.length];
-		int x = t;
-		int i = 0;
-		while(x != -1) {
-			path[i++] = x;
-			x = edgeTo[x];
-		}
-		return Arrays.copyOf(path, i);
+		return this.path;
 	}
 	
 	class Node implements Comparable<Node>{
 		int distance, index, capacity;
 		boolean marked;
-		int send, back;
 		public Node(int index, int distance, int capacity) {
 			this.distance = distance;
 			this.index = index;
@@ -190,9 +195,7 @@ class BFS {
 		@Override
 		public String toString() {
 			return "Node [distance=" + distance + ", index=" + index
-					+ ", capacity=" + capacity + ", marked=" + marked
-					+ ", send=" + send + ", back=" + back + "]";
+					+ ", capacity=" + capacity + ", marked=" + marked + "]";
 		}
-		
 	}
 }
