ea594d066e2c5282e281945cdb2cb8400e1b3bd3
wagsenyuan <wang.senyuan@gmail.com>
2013-11-17 20:42:11 +0800

    solution P28 (http://aperiodic.net/phil/scala/s-99/), it is a start

---
+++ ALG-S/src/test/s99/problems/p28/SolutionSuite.scala
@@ -0,0 +1,17 @@
+package problems.p28
+
+import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+
+@RunWith(classOf[JUnitRunner])
+class SolutionSuite extends FunSuite {
+	import Solution._
+  
+	test("lsortFreq") {
+	  val s = List(List('a, 'b, 'c), List('d, 'e), List('f, 'g, 'h), List('d, 'e), List('i, 'j, 'k, 'l), List('m, 'n), List('o))
+	  val t = List(List('i, 'j, 'k, 'l), List('o), List('a, 'b, 'c), List('f, 'g, 'h), List('d, 'e), List('d, 'e), List('m, 'n))
+	  assert(lsortFreq(s) == t, "lsortFreq")
+	}
+}
